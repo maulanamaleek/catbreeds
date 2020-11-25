dba26ee7760201178d8e03f33c4d684fb369277e
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-11-25 20:27:23 +0700

    adding components and style
    adding expand button, change component style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Card, Accordion } from 'react-bootstrap';
+import { Card, Accordion, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import InfiniteScroll from 'react-infinite-scroller';
 import Logo from './assets/logo.png';
@@ -17,12 +17,12 @@ class App extends Component {
   }
 
   componentDidMount(){
-    axios.get('https://api.thecatapi.com/v1/breeds?page=0&limit=15&api_key=7459a40e-be79-4420-bcd2-08c6c649bb6b')
+    axios.get('https://api.thecatapi.com/v1/breeds?page=0&limit=10&api_key=7459a40e-be79-4420-bcd2-08c6c649bb6b')
     .then(data => this.setState({cat: data.data}))
   };
 
   fetchMoreData = async () => {
-    const check = this.state.cat.length / 15;
+    const check = this.state.cat.length / 10;
     const load = await axios.get(`https://api.thecatapi.com/v1/breeds?page=${check}&limit=15&api_key=7459a40e-be79-4420-bcd2-08c6c649bb6b`)
                        .then(data=> data.data);
     const newData = [...this.state.cat, ...load];
@@ -40,25 +40,31 @@ class App extends Component {
     });
 
     const search = this.state.search;
-    const ite = this.state.cat.filter(i => i.name.toLowerCase().includes(search.toLowerCase()));
+    const item = this.state.cat.filter(i => i.name.toLowerCase().includes(search.toLowerCase()));
     const more = search.length ? false : true;
 
     this.setState({
-      result: ite,
+      result: item,
       hasmore: more,
+    })
+  }
+
+  click = () => {
+    this.setState({
+      search: "",
+      result: [],
+      hasMore: true,
     })
   }
 
   submit = (e) => {
     e.preventDefault();
     const search = this.state.search;
-    const ite = this.state.cat.filter(i => i.name.toLowerCase().includes(search.toLowerCase()));
+    const item = this.state.cat.filter(i => i.name.toLowerCase().includes(search.toLowerCase()));
     const more = search.length ? false : true;
 
-
-
     this.setState({
-      result: ite,
+      result: item,
       hasmore: more,
     })
   }
@@ -68,9 +74,13 @@ class App extends Component {
       <li className="list-item" key={item.id}>  
                   <Accordion style={{boxShadow: "1px 2px 5px rgba(0, 0, 0, 0.1)"}}>
                     <Card style={{backgroundColor: "#eee"}}>
-                      <Accordion.Toggle as={Card.Header} eventKey="0" style={{backgroundColor: "#76bbe9"}}>
+                      <Card.Header style={{backgroundColor: "#76bbe9"}}>
                         <b>{item.name}</b> - {item.origin}
+                        <Accordion.Toggle as={Button} eventKey="0" style={{backgroundColor: "#0278ae", cursor: "pointer", border: 0, float: "right", width: 100}}>
+                        Details
                       </Accordion.Toggle>
+                      </Card.Header>
+                      
                       <Accordion.Collapse eventKey="0">
                         <Card.Body>
                           <div className="toogle-items" key={item.id}>
@@ -99,9 +109,9 @@ class App extends Component {
       <div>
         <img className="logo" src={Logo} alt="Cat Breeds" />
         <div className="search">
-          <form onSubmit={this.submit}>
+          <form>
             <input type="text" placeholder="Search cat name" value={this.state.search} onChange={this.change} />
-            <button type="submit">Search</button>
+            <button onClick={this.click}>x</button>
           </form>
         </div>
         <InfiniteScroll
